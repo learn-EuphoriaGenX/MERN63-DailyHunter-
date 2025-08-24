dbb26520fbf518537173e29e9de50e45ab147116
ranjit-dey <ranjitdey05265@gmail.com>
2025-08-25 00:26:18 +0530

    Add homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import SubscripTtion from './pages/Subscription'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Navbar from './components/Navbar'
-import SIdebar from './components/SIdebar'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import AddTask from './pages/AddTask'
-import ResolvedTask from './pages/ResolvedTask'
-import Settings from './pages/Settings'
+import Navbar from "./components/Navbar";
+import SIdebar from "./components/SIdebar";
+
+import HomePage from "./pages/HomePage"
+import Dashboard from "./pages/Dashboard";
+import SubscripTtion from "./pages/Subscription";
+import AddTask from "./pages/AddTask";
+import ResolvedTask from "./pages/ResolvedTask";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
@@ -19,21 +21,19 @@ function App() {
       <div className="sm:ml-64">
         <div className="">
           <Routes>
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/subscription' element={<SubscripTtion />} />
-            <Route path='/add-task' element={<AddTask />} />
-            <Route path='/resolved-task' element={<ResolvedTask />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/subscription" element={<SubscripTtion />} />
+            <Route path="/add-task" element={<AddTask />} />
+            <Route path="/resolved-task" element={<ResolvedTask />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/settings" element={<Settings />} />
           </Routes>
         </div>
       </div>
-
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
